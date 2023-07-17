20915cc5d3c5791ae0ee2a17d1b7530fc7e1292e
TURPIN Jonathan <turpin.j@hotmail.fr>
2023-07-17 13:36:14 +0200

    update Gallery component

--- front/src/components/Galery/index.js
+++ front/src/components/Galery/index.js
@@ -1,158 +1,191 @@
-import { useState } from 'react';
-import BgImg from '@pictures/about.jpg';
-import { useTranslation } from 'react-i18next';
-// import Modal from 'react-modal';
+import BgImg from "@pictures/about.jpg";
+import { useTranslation } from "react-i18next";
+import { Gallery } from "react-grid-gallery";
 
-
-import Relaxation from '@galery/relaxation.jpg';
-import Acostage from '@galery/acostage.jpg';
-import Contemplation from '@galery/contemplation.jpg';
-import Conviviality from '@galery/conviviality.jpg';
-import Discovery from '@galery/discovery.jpg';
-import Escape from '@galery/escape.jpg';
-import Immensity from '@galery/immensity.jpg';
-import MadeToMesure from '@galery/made_to_mesure.jpg';
-import Marina from '@galery/marina.jpg';
-import Romantic from '@galery/romantic.jpg';
-import Sunset from '@galery/sunset.jpg';
-import Swimming from '@galery/swimming.jpg';
+import Relaxation from "@galery/relaxation.jpg";
+import Acostage from "@galery/acostage.jpg";
+import Contemplation from "@galery/contemplation.jpg";
+import Conviviality from "@galery/conviviality.jpg";
+import Discovery from "@galery/discovery.jpg";
+import Escape from "@galery/escape.jpg";
+import Immensity from "@galery/immensity.jpg";
+import MadeToMesure from "@galery/made_to_mesure.jpg";
+import Marina from "@galery/marina.jpg";
+import Romantic from "@galery/romantic.jpg";
+import Sunset from "@galery/sunset.jpg";
+import Swimming from "@galery/swimming.jpg";
 
 const Galery = () => {
   const { t } = useTranslation();
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [selectedPicture, setSelectedPicture] = useState(null);
-
-  const openModal = (picture) => {
-    setSelectedPicture(picture);
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setSelectedPicture(null);
-    setModalIsOpen(false);
-  };
-
 
   const pictures = [
     {
       id: 1,
-      path: Relaxation,
-      title: t('galery.title.relaxation'),
-      description: t('galery.description.relaxation'),
-      key: 'relaxation'
+      src: Relaxation,
+      caption: t("galery.title.relaxation"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.relaxation")}</div>
+        </div>
+      ),
+      alt: t("galery.description.relaxation"),
+      key: "relaxation",
+      width: 623,
+      height: 831,
     },
     {
       id: 2,
-      path: Discovery,
-      title: t('galery.title.discovery'),
-      description: t('galery.description.discovery'),
-      key: 'discovery'
+      src: Discovery,
+      caption: t("galery.title.discovery"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.discovery")}</div>
+        </div>
+      ),
+      alt: t("galery.description.discovery"),
+      key: "discovery",
+      width: 320,
+      height: 400,
     },
     {
       id: 3,
-      path: Acostage,
-      title: t('galery.title.acostage'),
-      description: t('galery.description.acostage'),
-      key: 'acostage'
+      src: Acostage,
+      caption: t("galery.title.acostage"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.acostage")}</div>
+        </div>
+      ),
+      alt: t("galery.description.acostage"),
+      key: "acostage",
+      width: 320,
+      height: 400,
     },
     {
       id: 4,
-      path: Escape,
-      title: t('galery.title.escape'),
-      description: t('galery.description.escape'),
-      key: 'escape'
+      src: Escape,
+      caption: t("galery.title.escape"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.escape")}</div>
+        </div>
+      ),
+      alt: t("galery.description.escape"),
+      key: "escape",
+      width: 320,
+      height: 400,
     },
     {
       id: 5,
-      path: Sunset,
-      title: t('galery.title.sunset'),
-      description: t('galery.description.sunset'),
-      key: 'sunset'
-    },
-    {
-      id: 6,
-      path: Marina,
-      title: t('galery.title.marina'),
-      description: t('galery.description.marina'),
-      key: 'marina'
-    },
-    {
-      id: 7,
-      path: Romantic,
-      title: t('galery.title.romantic'),
-      description: t('galery.description.romantic'),
-      key: 'romantic'
-    },
-    {
-      id: 8,
-      path: Swimming,
-      title: t('galery.title.swimming'),
-      description: t('galery.description.swimming'),
-      key: 'swimming'
+      src: Sunset,
+      caption: t("galery.title.sunset"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.sunset")}</div>
+        </div>
+      ),
+      alt: t("galery.description.sunset"),
+      key: "sunset",
+      width: 320,
+      height: 400,
     },
     {
       id: 9,
-      path: Contemplation,
-      title: t('galery.title.contemplation'),
-      description: t('galery.description.contemplation'),
-      key: 'contemplation'
+      src: Contemplation,
+      caption: t("galery.title.contemplation"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.contemplation")}</div>
+        </div>
+      ),
+      alt: t("galery.description.contemplation"),
+      key: "contemplation",
+    },
+    {
+      id: 6,
+      src: Marina,
+      caption: t("galery.title.marina"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.marina")}</div>
+        </div>
+      ),
+      alt: t("galery.description.marina"),
+      key: "marina",
+    },
+    {
+      id: 7,
+      src: Romantic,
+      caption: t("galery.title.romantic"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.romantic")}</div>
+        </div>
+      ),
+      alt: t("galery.description.romantic"),
+      key: "romantic",
+    },
+    {
+      id: 8,
+      src: Swimming,
+      caption: t("galery.title.swimming"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.swimming")}</div>
+        </div>
+      ),
+      alt: t("galery.description.swimming"),
+      key: "swimming",
     },
     {
       id: 10,
-      path: Immensity,
-      title: t('galery.title.immensity'),
-      description: t('galery.description.immensity'),
-      key: 'immensity'
+      src: Immensity,
+      caption: t("galery.title.immensity"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.immensity")}</div>
+        </div>
+      ),
+      alt: t("galery.description.immensity"),
+      key: "immensity",
     },
     {
       id: 11,
-      path: Conviviality,
-      title: t('galery.title.conviviality'),
-      description: t('galery.description.conviviality'),
-      key: 'conviviality'
+      src: Conviviality,
+      caption: t("galery.title.conviviality"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.conviviality")}</div>
+        </div>
+      ),
+      alt: t("galery.description.conviviality"),
+      key: "conviviality",
     },
     {
       id: 12,
-      path: MadeToMesure,
-      title: t('galery.title.made_to_mesure'),
-      description: t('galery.description.made_to_mesure'),
-      key: 'made_to_mesure'
-    }
-
-  ]
+      src: MadeToMesure,
+      caption: t("galery.title.made_to_mesure"),
+      customOverlay: (
+        <div className="custom-overlay_caption">
+          <div>{t("galery.title.made_to_mesure")}</div>
+        </div>
+      ),
+      alt: t("galery.description.made_to_mesure"),
+      key: "made_to_mesure",
+    },
+  ];
 
   return (
     <main id="galery" style={{ backgroundImage: `url(${BgImg})` }}>
       <div className="galery_container">
-        {pictures.map((picture, index) => (
-          <>
-            <img
-              key={index}
-              id={picture.key}
-              onClick={() => openModal(picture)}
-              src={picture.path} alt={picture.description}
-              className='galery_item'
-            />
-          </>
-        ))}
+        <Gallery
+          className="gallery"
+          images={pictures}
+          enableImageSelection={false}
+          rowHeight="250px"
+        />
       </div>
-      {/* <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Image Modal"
-        className="modal"
-        overlayClassName="overlay"
-      >
-        {selectedPicture && (
-          <div>
-            <img src={selectedPicture.path} alt={selectedPicture.description} />
-            <h2>{selectedPicture.title}</h2>
-            <p>{selectedPicture.description}</p>
-            <button className="modal-close" onClick={closeModal}>Close</button>
-          </div>
-        )}
-      </Modal> */}
     </main>
   );
-}
+};
 export default Galery;
