958f6808a21da4ca805912e9055e0074a9815908
TURPIN Jonathan <turpin.j@hotmail.fr>
2023-07-23 15:57:43 +0200

    navigation media done

--- front/src/components/Layout/navigation.js
+++ front/src/components/Layout/navigation.js
@@ -39,66 +39,63 @@ const Navigation = () => {
       </NavLink>
       <nav>
         <ul onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-          <li className={isHovered ? "hovered" : ""}>
+          <li className={isHovered ?? "hovered"}>
             <NavLink to="/about" end>
               <FontAwesomeIcon icon={faUser} className="iconNav" />
               {isHovered && <span>{t("nav.about")}</span>}
             </NavLink>
           </li>
-          <li className={isHovered ? "hovered" : ""}>
+          <li className={isHovered ?? "hovered"}>
             <NavLink to="/boat" end>
               <FontAwesomeIcon icon={faAnchor} className="iconNav" />
               {isHovered && <span>{t("nav.boat")}</span>}
             </NavLink>
           </li>
-          <li className={isHovered ? "hovered" : ""}>
+          <li className={isHovered ?? "hovered"}>
             <NavLink to="/tour" end>
               <FontAwesomeIcon icon={faSailboat} className="iconNav" />
               {isHovered && <span>{t("nav.tour")}</span>}
             </NavLink>
           </li>
-          <li className={isHovered ? "hovered" : ""}>
+          <li className={isHovered ?? "hovered"}>
             <NavLink to="/activities" end>
               <FontAwesomeIcon icon={faBinoculars} className="iconNav" />
               {isHovered && <span>{t("nav.doing")}</span>}
             </NavLink>
           </li>
-          <li className={isHovered ? "hovered" : ""}>
+          <li className={isHovered ?? "hovered"}>
             <NavLink to="/eat" end>
               <FontAwesomeIcon icon={faUtensils} className="iconNav" />
               {isHovered && <span>{t("nav.eat")}</span>}
             </NavLink>
           </li>
-          <li className={isHovered ? "hovered" : ""}>
+          <li className={isHovered ?? "hovered"}>
             <NavLink to={"/sleep"} end>
               <FontAwesomeIcon icon={faBed} className="iconNav" />
               {isHovered && <span>{t("nav.sleeping")}</span>}
             </NavLink>
           </li>
-          <li className={isHovered ? "hovered" : ""}>
+          <li className={isHovered ?? "hovered"}>
             <NavLink to={"/gallery"} end>
               <FontAwesomeIcon icon={faCamera} className="iconNav" />
               {isHovered && <span>{t("nav.gallery")}</span>}
             </NavLink>
           </li>
-          <li className={isHovered ? "hovered" : ""}>
+          <li className={isHovered ?? "hovered"}>
             <NavLink to={"/contact"} end>
               <FontAwesomeIcon icon={faAt} className="iconNav" />
               {isHovered && <span>{t("nav.contacts")}</span>}
             </NavLink>
           </li>
-          <li className={isHovered ? "hovered" : ""}>
-            <Toggle isHovered={isHovered} />
-          </li>
           {isHovered && (
             <>
-              <li className={isHovered ? "hovered" : ""}>
+              <li className={isHovered ?? "hovered"}>
                 <NavLink to={"/cancellation_policy"} end>
                   <FontAwesomeIcon icon={faPowerOff} className="iconNav" />
                   {isHovered && <span>{t("nav.cancellation")}</span>}
                 </NavLink>
               </li>
-              <li className={isHovered ? "hovered" : ""}>
+              <li className={isHovered ?? "hovered"}>
                 <a
                   href="https://www.livroreclamacoes.pt/Inicio/"
                   target="_blank"
@@ -113,6 +110,9 @@ const Navigation = () => {
               </li>
             </>
           )}
+          <li className={isHovered ?? "hovered"}>
+            <Toggle isHovered={isHovered} />
+          </li>
         </ul>
       </nav>
     </div>
