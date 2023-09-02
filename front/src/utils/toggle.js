736610671ad0d19cfe79affca950d478e1b73264
TURPIN Jonathan <turpin.j@hotmail.fr>
2023-09-02 10:07:06 +0200

    toggle language work on all views

--- front/src/utils/toggle.js
+++ front/src/utils/toggle.js
@@ -1,28 +1,71 @@
 import Fr from "@pictures/flag_france.png";
 import Pt from "@pictures/flag_portugal.png";
 import En from "@pictures/flag_england.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import i18n from "../i18n";
 import { useTranslation } from "react-i18next";
+import useResponsive from "@hooks/useResponsive";
 
-const Toggle = ({ setIsHovered }) => {
+const Toggle = ({ isHovered, setIsHovered }) => {
   const { t } = useTranslation();
 
   const [activeLang, setActiveLang] = useState("pt");
   const [showFlags, setShowFlags] = useState(false);
+  const isMobile = useResponsive(850);
 
+  // Function to handle language change when a flag is clicked
   const handleToggle = (lang) => {
     if (lang !== activeLang) {
       setActiveLang(lang);
       i18n.changeLanguage(lang);
     }
-    setShowFlags(!showFlags);
+    if (isMobile) {
+      setShowFlags(!showFlags);
+    }
+    // Set isHovered to false after clicking a flag
     setIsHovered(false);
   };
 
+  // Show flags on mouse enter if not in mobile view
+  const handleMouseEnter = () => {
+    if (!isMobile) {
+      setShowFlags(true);
+    }
+  };
+
+  // Function to handle mouse leave event
+  const handleMouseLeave = () => {
+    // Hide flags on mouse leave if not in mobile view
+    if (!isMobile) {
+      setShowFlags(false);
+    }
+    // Set isHovered to false on mouse leave
+    setIsHovered(false);
+  };
+
+  // Effect to control when to show flags
+  useEffect(() => {
+    // Show flags when hovered and not in mobile view
+    if (isHovered && !isMobile) {
+      setShowFlags(true);
+    } else {
+      // Hide flags in other cases
+      setShowFlags(false);
+    }
+  }, [isHovered, isMobile]);
+
   return (
-    <div id="toggle">
-      {showFlags ? (
+    <div
+      id="toggle"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={() => {
+        if (isMobile) {
+          setShowFlags(!showFlags);
+        }
+      }}
+    >
+      {showFlags || (!isMobile && isHovered) ? (
         <div>
           <img
             src={Fr}
